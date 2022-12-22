84e18cb75fa283a7c5e12b6715d9907371ae6bce
Abhijit Bhattacharjee <abhijitb1@gmail.com>
2022-12-22 11:04:01 -0800

    First working build

---
+++ customzsh.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+## install required toolchain
+sudo apt install -y zsh zsh-doc git curl
+
+## get oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## get zsh plugins
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+## edit default zshrc file
+# get default zsh template
+#wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/templates/zshrc.zsh-template -O ~/.zshrc
+
+
+# change theme to agnoster
+#sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
+
+# add plugins to list
+#sed -i 's/plugins=(git)/plugins=(\ngit\nzsh-syntax-highlighting\nzsh-autosuggestions)/g' ~/.zshrc
+
+## copy custom zshrc file to current user dir
+cp .zshrc ~/
+
+## activate zsh with new defaults
+source ~/.zshrc
